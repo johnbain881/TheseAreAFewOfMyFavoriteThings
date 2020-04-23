2dc7e0ccf5ac325425e915a7ce1fcce1be37635e
John Bain <johnknoxbain@gmail.com>
2020-04-23 14:01:07 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const submit = document.getElementById("submit")
+const thing = document.getElementById("thing")
+const location1 = document.getElementById("location")
+const output = document.getElementById("output")
+
+submit.addEventListener("click", function () {
+    output.innerHTML = `I can purchase ${thing.value} at ${location1.value}`
+})
