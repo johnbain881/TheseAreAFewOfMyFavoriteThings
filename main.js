e3b4ab82cc2d128d9e69445a6ced8d9ce1a3fb1c
John Bain <johnknoxbain@gmail.com>
2020-04-23 14:13:36 -0500

    challenge 2 completed

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ const location1 = document.getElementById("location")
 const output = document.getElementById("output")
 
 submit.addEventListener("click", function () {
-    output.innerHTML = `I can purchase ${thing.value} at ${location1.value}`
-    thing.value = "";
-    location1.value = "";
-    thing.focus();
-    thing.select();
+    if (thing.value === "" || location1.value === "") {
+        alert("All information needs to be entered")
+    } else {
+        output.innerHTML = `I can purchase ${thing.value} at ${location1.value}`
+        thing.value = "";
+        location1.value = "";
+        thing.focus();
+        thing.select();
+    }
 })
